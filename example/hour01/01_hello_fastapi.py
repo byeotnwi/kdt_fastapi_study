44d5e711dd1453e91988cb8622e70aefb940851f
byeotnwi <hankh7214@gmail.com>
2025-11-14 10:06:22 +0900

    first commit

---
+++ example/hour01/01_hello_fastapi.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+
+app = FastAPI(docs_url='/api/docs', openapi_url='/api/openapi.json')
+
+@app.get("/hello")
+def say_hello() -> dict:
+    """간단한 JSON 메세지를 반환합니다."""
+    return {"message": "Hello, FastAPI learners!"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8888)
